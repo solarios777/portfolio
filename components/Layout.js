1d3173e0ccb50a05db9d70e87eaf0dbb48e6e480
solarios777 <tsegasolomon635@gmail.com>
2024-08-14 11:51:07 +0300

    layout completed

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,27 @@
-
 // fonts
-import { Sora, Sora } from "next/font/google"
+import { Sora } from "next/font/google";
 
-// font setting
-const Sora=Sora
+// font settings
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
-const Layout = () => {
-  return <div>Layout</div>;
+// components
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-custom-bg text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
+      <TopLeftImg/>
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  )
 };
 
 export default Layout;
