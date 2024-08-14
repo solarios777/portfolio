d5ba49cd045347092f140d9ab9b989fa3972b4d6
solarios777 <tsegasolomon635@gmail.com>
2024-08-14 06:11:02 +0300

    file and folder structure

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ethan-portfolio-starter
+# solomon portfolio
---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+
+// fonts
+import { Sora, Sora } from "next/font/google"
+
+// font setting
+const Sora=Sora
+
+const Layout = () => {
+  return <div>Layout</div>;
+};
+
+export default Layout;
