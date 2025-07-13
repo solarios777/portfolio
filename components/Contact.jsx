20d4be8e5541c267283d60b00889cefcde4dc6be
solomon <tsegasolomon635@gmail.com>
2025-07-13 13:58:51 +0300

    personal info removed

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,13 +1,13 @@
 import { FaPhone, FaPaperPlane } from "react-icons/fa";
 
 const ContactPage = () => {
-  const handleEmailOpen = () => {
-    window.location.href = "mailto:tsegasolomon635@gmail.com";
-  };
+  // const handleEmailOpen = () => {
+  //   window.location.href = "mailto:tsegasolomon635@gmail.com";
+  // };
 
-  const handlePhoneCall = () => {
-    window.location.href = "tel:+251938838999";
-  };
+  // const handlePhoneCall = () => {
+  //   window.location.href = "tel:+251938838999";
+  // };
 
   return (
     <div className="flex justify-center items-center text-center bg-[rgba(28,20,54,0.15)] text-white pt-20 container mx-auto px-4 sm:px-6 lg:px-8">
@@ -24,21 +24,21 @@ const ContactPage = () => {
         <div className="shadow-md rounded-lg p-8">
           <button
             className="px-4 py-2 flex items-center  gap-x-8 rounded-md bg-gray-800 text-white hover:bg-gray-900 font-medium transition-colors mx-auto border-2 border-[#9088af] border-opacity-90 "
-            onClick={handleEmailOpen}
+            // onClick={handleEmailOpen}
           >
             let&apos;s get in touch
             <FaPaperPlane />
           </button>
 
           <div className="font-medium mx-auto mt-6 flex items-center justify-center gap-x-8 flex-col md:flex-row">
-            Phone: +251 938 838
-            999
+            {/* Phone: +251 938 838
+            999 */}
             <button
               className="px-4 py-2 flex rounded-md text-white hover:bg-gray-900 font-medium transition-colors border-2 border-[#9088af] border-opacity-90 lg:border-none"
-              onClick={handlePhoneCall}
+              // onClick={handlePhoneCall}
             >
               <FaPhone className="text-[#25D366] mr-3" />
-              Call Me
+              {/* Call Me */}
             </button>
           </div>
         </div>
--- components/Header.js
+++ components/Header.js
@@ -90,7 +90,7 @@ const Header = () => {
           </Link>
           <div className="flex items-center gap-x-5 text-lg fixed top-4 lg:top-6 right-4 lg:right-24 z-50 px-4 py-2 rounded-md z-40">
             <Link
-              href={"https://www.linkedin.com/in/solomon-sala-b86577323/"}
+              // href={"https://www.linkedin.com/in/solomon-sala-b86577323/"}
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-accent transition-all duration-300 lg:text-2xl"
@@ -98,7 +98,7 @@ const Header = () => {
               <RiLinkedinFill className="text-[#0A66C2]" />
             </Link>
             <Link
-              href={"https://github.com/solarios777"}
+              // href={"https://github.com/solarios777"}
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-accent transition-all duration-300 lg:text-2xl"
@@ -107,7 +107,7 @@ const Header = () => {
             </Link>
 
             <Link
-              href={"https://wa.me/+251938838999"}
+              // href={"https://wa.me/+251938838999"}
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-accent transition-all duration-300 lg:text-2xl"
