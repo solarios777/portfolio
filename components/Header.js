c267f583b5a6b611168128219b0291efe33bb074
solarios777 <tsegasolomon635@gmail.com>
2026-03-13 19:31:34 +0300

    contact info added

--- components/Header.js
+++ components/Header.js
@@ -90,7 +90,7 @@ const Header = () => {
           </Link>
           <div className="flex items-center gap-x-5 text-lg fixed top-4 lg:top-6 right-4 lg:right-24 z-50 px-4 py-2 rounded-md z-40">
             <Link
-              href={"/#contact"}
+              href={"https://www.linkedin.com/in/solomon-sala-0536b53b6"}
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-accent transition-all duration-300 lg:text-2xl"
@@ -98,7 +98,7 @@ const Header = () => {
               <RiLinkedinFill className="text-[#0A66C2]" />
             </Link>
             <Link
-              href={"/#contact"}
+              href={"https://github.com/solarios777"}
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-accent transition-all duration-300 lg:text-2xl"
@@ -107,7 +107,7 @@ const Header = () => {
             </Link>
 
             <Link
-             href={"/#contact"}
+              href={"/#contact"}
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-accent transition-all duration-300 lg:text-2xl"
